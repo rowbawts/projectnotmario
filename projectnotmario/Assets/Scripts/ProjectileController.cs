94480cbad21e5d1cb6915f13d2a0d72c03cb48b9
okseby <okseby@protonmail.com>
2023-06-13 21:34:34 -0400

    Add collision detection for all cases.

--- projectnotmario/Assets/Scripts/ProjectileController.cs
+++ projectnotmario/Assets/Scripts/ProjectileController.cs
@@ -6,12 +6,24 @@ public class ProjectileController : MonoBehaviour
 {
     public float launchForce = 10f;
     public Vector3 direction = Vector3.forward;
-    private Rigidbody bullet;
+    public Vector3 center = Vector3.zero;
+    public Vector3 size = Vector3.one;
+    private Rigidbody projectile;
     
-    void Awake()
+    private void Awake()
     {
-        bullet = GetComponent<Rigidbody>();
+        projectile = GetComponent<Rigidbody>();
 
-        bullet.AddForce(direction * launchForce, ForceMode.Impulse);
+        projectile.AddForce(direction * launchForce, ForceMode.Impulse);
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        // Check if the collision involves a specific tag
+        if (collision.gameObject.CompareTag("Player") || collision.gameObject.CompareTag("Enemy") || collision.gameObject.CompareTag("Bullet"))
+        {
+            // Code to execute when collision occurs
+            Destroy(gameObject);
+        }
     }
 }
--- projectnotmario/Assets/Scripts/ShipController.cs
+++ projectnotmario/Assets/Scripts/ShipController.cs
@@ -1,12 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 // ChatGPT made this lol
 public class ShipController : MonoBehaviour
 {
     public float moveSpeed = 5f;
     public float fireRate = 0.5f;
+    public int health = 3;
+    public Text healthText;
     public GameObject cannon;
     public GameObject bulletPrefab;
     private Vector3 bulletSpawnVector;
@@ -14,18 +17,28 @@ public class ShipController : MonoBehaviour
     private float nextFireTime;
     private Rigidbody ship;
 
-    private void Awake() {
+    private void Awake()
+    {
         // Get ship's rigidbody component
         ship = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
-    private void Update() {
+    private void Update()
+    {
         move();
         shoot();
+
+        healthText.text = "Health: " + health.ToString();
+
+        if (health == 0)
+        {
+            Destroy(gameObject);
+        }
     }
 
-    private void move() {
+    private void move()
+    {
         // Get input axes
         Vector3 playerInput = new Vector3(Input.GetAxisRaw("Horizontal"), 0f, Input.GetAxisRaw("Vertical"));
 
@@ -36,7 +49,8 @@ public class ShipController : MonoBehaviour
         ship.MovePosition(ship.position + moveVector * Time.fixedDeltaTime);
     }
 
-    private void shoot() {
+    private void shoot()
+    {
         if (Input.GetKey(KeyCode.Space) && Time.time >= nextFireTime) {
             // Create vector to spawn bullet and tip of cannon
             bulletSpawnVector = new Vector3(cannon.transform.position.x, 0f, cannon.transform.position.z);
@@ -45,6 +59,16 @@ public class ShipController : MonoBehaviour
             GameObject spawnedPrefab = Instantiate(bulletPrefab, bulletSpawnVector, Quaternion.identity);
 
             nextFireTime = Time.time + fireRate;
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        // Check if the collision involves a specific tag
+        if (collision.gameObject.CompareTag("Enemy") || collision.gameObject.CompareTag("Bullet"))
+        {
+            // Code to execute when collision occurs
+            health -= 1;
         }
     }
 }
